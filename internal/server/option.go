83cdb3bf475ab9162b2b32d2a743d3e3df53f2ac
murasame29 <oogiriminister@gmail.com>
2024-03-17 16:50:19 +0000

    graceful shutdownの実装をerror groupに修正

--- internal/server/option.go
+++ internal/server/option.go
@@ -6,53 +6,53 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type Option func(*server)
+type Option func(*Server)
 
 // WithPort はポート番号を設定するオプションです。
 func WithPort(port int) Option {
-	return func(s *server) {
+	return func(s *Server) {
 		s.port = port
 	}
 }
 
 // WithHost はホスト名を設定するオプションです。
 func WithHost(host string) Option {
-	return func(s *server) {
+	return func(s *Server) {
 		s.host = host
 	}
 }
 
 // WithLogger はロガーを設定するオプションです。
 func WithLogger(l *logrus.Logger) Option {
-	return func(s *server) {
+	return func(s *Server) {
 		s.l = l
 	}
 }
 
 // WithReadTimeout はリクエストの読み込みタイムアウトを設定するオプションです。
 func WithReadTimeout(timeout time.Duration) Option {
-	return func(s *server) {
+	return func(s *Server) {
 		s.srv.ReadTimeout = timeout
 	}
 }
 
 // WithWriteTimeout はレスポンスの書き込みタイムアウトを設定するオプションです。
 func WithWriteTimeout(timeout time.Duration) Option {
-	return func(s *server) {
+	return func(s *Server) {
 		s.srv.WriteTimeout = timeout
 	}
 }
 
 // WithIdleTimeout はアイドルタイムアウトを設定するオプションです。
 func WithIdleTimeout(timeout time.Duration) Option {
-	return func(s *server) {
+	return func(s *Server) {
 		s.srv.IdleTimeout = timeout
 	}
 }
 
 // WithShutdownTimeout はシャットダウンタイムアウトを設定するオプションです。
 func WithShutdownTimeout(timeout time.Duration) Option {
-	return func(s *server) {
+	return func(s *Server) {
 		s.shutdownTimeout = timeout
 	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,16 +4,17 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/sync/errgroup"
 )
 
 const DefaultShutdownTimeout time.Duration = 10
 
-type server struct {
+type Server struct {
 	port            int
 	host            string
 	shutdownTimeout time.Duration
@@ -23,17 +24,9 @@ type server struct {
 	srv *http.Server
 }
 
-// Server はサーバーを表すインターフェースです。
-type Server interface {
-	Run() error
-	Shutdown(ctx context.Context) error
-
-	RunWithGracefulShutdown()
-}
-
 // New はサーバーを生成します。
-func New(handler http.Handler, opts ...Option) Server {
-	server := &server{
+func New(handler http.Handler, opts ...Option) *Server {
+	server := &Server{
 		port:            8080,
 		host:            "localhost",
 		shutdownTimeout: DefaultShutdownTimeout,
@@ -54,36 +47,45 @@ func New(handler http.Handler, opts ...Option) Server {
 }
 
 // Run はサーバーを起動します。
-func (s *server) Run() error {
+func (s *Server) Run() error {
 	s.l.Infof("server starting at %s", s.srv.Addr)
 	return s.srv.ListenAndServe()
 }
 
 // Shutdown はサーバーを停止します。
-func (s *server) Shutdown(ctx context.Context) error {
+func (s *Server) Shutdown(ctx context.Context) error {
 	s.l.Infof("server shutdown ...")
 	return s.srv.Shutdown(ctx)
 }
 
 // RunWithGracefulShutdown はgraceful shutdownを行うサーバーを起動します。
-func (s *server) RunWithGracefulShutdown() {
-	go func() {
+func (s *Server) RunWithGracefulShutdown() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGALRM)
+	defer stop()
+
+	errWg, errCtx := errgroup.WithContext(ctx)
+	errWg.Go(func() error {
 		if err := s.Run(); err != nil && err != http.ErrServerClosed {
-			s.l.Fatalf("Listen And Serve error : %s", err.Error())
+			return fmt.Errorf("Listen And Serve error : %s", err.Error())
 		}
-	}()
 
-	s.l.Infof("server starting at %s", s.srv.Addr)
+		return nil
+	})
 
-	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
-	defer cancel()
+	errWg.Go(func() error {
+		<-errCtx.Done()
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
+		ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+		defer cancel()
 
-	<-quit
-	if err := s.Shutdown(ctx); err != nil {
-		s.l.Fatalf("server shutdown error : %s", err.Error())
+		return s.Shutdown(ctx)
+	})
+
+	err := errWg.Wait()
+
+	if err != context.Canceled &&
+		err != nil {
+		s.l.Error(err)
 	}
 
 	s.l.Infof("server shutdown completed")
