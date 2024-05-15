215cd1549b549b4b040851c1d18279766e7f86c4
murasame29 <oogiriminister@gmail.com>
2024-05-15 14:29:38 +0000

    NewRelicの環境変数を削除

--- cmd/config/type.go
+++ cmd/config/type.go
@@ -9,11 +9,6 @@ type config struct {
 		Port            int           `env:"PORT" envDefault:"8080"`
 		ShutdownTimeout time.Duration `env:"SHUTDOWN_TIMEOUT" envDefault:"10s"`
 	}
-
-	NewRelic struct {
-		AppName string `env:"NEW_RELIC_APP_NAME" envDefault:"golang-backend-server"`
-		License string `env:"NEW_RELIC_LICENSE" envDefault:""`
-	}
 }
 
 // Config は読み込まれた設定を保持します。
