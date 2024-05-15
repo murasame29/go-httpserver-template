c40a4480a2da058ab791035c7d8692541a87522c
murasame29 <oogiriminister@gmail.com>
2024-05-15 14:29:59 +0000

    依存関係のバージョンアップ && NewRelicの削除

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,26 +1,27 @@
 package config
 
 import (
-	"github.com/caarlos0/env"
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
-func LoadEnv(path ...string) {
-	if path != nil {
+func LoadEnv(path ...string) error {
+	if len(path) > 0 {
 		if err := godotenv.Load(path...); err != nil {
-			panic(err)
+			return fmt.Errorf("failed to load env: %w", err)
 		}
 	}
 
 	config := &config{}
 
 	if err := env.Parse(&config.Server); err != nil {
-		panic(err)
+		return err
 	}
 
-	if err := env.Parse(&config.NewRelic); err != nil {
-		panic(err)
-	}
-
+	// ... configに構造体を追加したら env.Parseする
 	Config = config
+
+	return nil
 }
